4cd4ec26616c1feb7d5d18f907f676b54a83beb6
Ash <rickvdhh@gmail.com>
2022-05-13 23:50:26 +0200

    Release 1.1.0
    Release "1.1.0",
    
    Added OS System TItle. Made the gen faster by x2!
    
    Rinck
    13/05/2022
    
    Co-Authored-By: slifxie <104720630+slifxie@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
 import time
+import os
+
+os.system('TITLE Rinck#0001 » Rinck-Nitro-Generator')
 
 characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
@@ -10,7 +13,7 @@ while True:
         randomNitro = f"{randomNitro}{random.choice(characters)}"
 
     print(f"Rinck » https://discord.gift/{randomNitro}") 
-    time.sleep(0.01)
+    time.sleep(0.005)
 
     with open("./Codes/codes.txt", "a+") as codesF:
 
