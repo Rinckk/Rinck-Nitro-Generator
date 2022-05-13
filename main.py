d44932db54f440dbea588c7b17e0d357b9063805
Ash <91659765+Rinckk@users.noreply.github.com>
2022-05-14 01:49:18 +0200

    1.1.0 Update
    Changed "randomNitro" to "Nitro"
    
    Rinck
    14/05/2022

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ os.system('TITLE Rinck#0001 » Rinck-Nitro-Generator')
 characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 while True:
-    randomNitro = ''
+    Nitro = ''
 
     for i in range(16):
-        randomNitro = f"{randomNitro}{random.choice(characters)}"
+        Nitro = f"{Nitro}{random.choice(characters)}"
 
-    print(f"Rinck » https://discord.gift/{randomNitro}") 
+    print(f"Rinck » https://discord.gift/{Nitro}") 
     time.sleep(0.001)
 
     with open("./Codes/codes.txt", "a+") as codesF:
 
-        codesF.write(f"Rinck#0001 » https://discord.gift/{randomNitro}\n")
+        codesF.write(f"Rinck#0001 » https://discord.gift/{Nitro}\n")
 
         codesF.close()
